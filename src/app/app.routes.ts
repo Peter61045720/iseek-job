4ca2deff98b41518ddaeaf76b273c11effbde6c7
Peter61045720 <peter61045720@gmail.com>
2025-03-21 15:34:19 +0100

    Create login and registration page; Implement basic routing; Setup Angular Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(p => p.LoginPage)
+  },
+  {
+    path: 'registration',
+    loadComponent: () => import('./pages/registration/registration.page').then(p => p.RegistrationPage)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
