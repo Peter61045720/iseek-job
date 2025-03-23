ebc4f0a0f241e5bd3353e07b61931bfeb063882c
Peter61045720 <peter61045720@gmail.com>
2025-03-23 21:27:32 +0100

    Create a basic registration form

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { passwordMatchValidator } from '../../shared/validators/password-match.validator';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatError,
+    MatCard,
+    MatCardHeader,
+    MatCardActions,
+    MatCardTitle,
+    MatCardContent,
+  ],
   templateUrl: './registration.page.html',
-  styleUrl: './registration.page.scss'
+  styleUrl: './registration.page.scss',
 })
 export class RegistrationPage {
+  registrationForm = new FormGroup(
+    {
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    },
+    passwordMatchValidator('password', 'confirmPassword')
+  );
 
+  get isFormValid(): boolean {
+    return this.registrationForm.valid;
+  }
+
+  register(): void {
+    console.log(this.registrationForm.get('username')?.value);
+    console.log(this.registrationForm.get('email')?.value);
+    console.log(this.registrationForm.get('password')?.value);
+  }
 }
