d8c38921b58154cf2fb8989bd0dafa13ebd59623
Peter61045720 <peter61045720@gmail.com>
2025-04-22 16:23:27 +0200

    Adding content to the home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, LayoutModule, MatCardModule, MatIcon],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
-export class HomePage {}
+export class HomePage implements OnInit, OnDestroy {
+  subscription: Subscription | null = null;
+  cols = 2;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.subscription = this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.cols = result.matches ? 1 : 3;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
